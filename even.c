abf1976e136e31db3d67a4470723460d5160689f
namankumar818 <80568028+namankumar818@users.noreply.github.com>
2021-04-11 23:43:30 -0700

    even_odd number check.c

---
+++ even.c
@@ -0,0 +1,27 @@
+//even odd
+
+#include<stdio.h>
+void main()
+{
+    int num;
+    printf("enter the number");
+    scanf("%d", &num);
+  if(num % 2 == 0)
+   printf("number is even");
+   else
+      printf("number is odd");
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
