56b4a4cdc42852880280e62656ca399f11c2ac01
namankumar818 <80568028+namankumar818@users.noreply.github.com>
2021-04-11 23:57:30 -0700

    doller coverter, if 1 doller is72 rupees.c

---
+++ doller.c
@@ -0,0 +1,24 @@
+// doller converter
+#include<stdio.h>
+void main()
+{
+  float  doller, rupees;
+   printf("enetr the amount in dollar");
+   scanf("%f",&doller);
+   rupees = doller*72;
+   
+  printf("%.2f rupees", rupees); 
+
+
+
+
+
+}
+  
+
+
+
+
+
+
+
